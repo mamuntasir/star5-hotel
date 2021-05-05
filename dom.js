ae64ee51eff54ea60d93a0ce922cb6a1499f80d2
asif-96 <shuyeb.dk@gmail.com>
2021-05-06 00:43:13 +0600

    add all files of hotel site

---
+++ dom.js
@@ -0,0 +1,99 @@
+var v;
+
+// v = document;
+// v = document.all;
+// v = document.all[9];
+// v = document.head;
+// v = document.body;
+// v = document.title;
+// v = document.links;
+// v = document.links[0];
+// v = document.images;
+// v = document.images[0];
+// v = document.forms;
+// v = document.forms[0];
+// v = document.doctype;
+// v = document.URL;
+// v = document.domain;
+// v = document.baseURI;
+
+// GET Attribute
+// v = document.getElementById("header").innerHTML;
+// v = document.getElementById("header").innerText;
+// v = document.getElementById("header").getAttribute("onClick");
+// v = document.getElementById("header").getAttributeNode("style"); 
+// v = document.getElementById("header").attributes;
+// v = document.getElementById("header").attributes[3];
+// v = document.getElementById("header").attributes[3].value;
+// v = document.getElementById("header").attributes[3].name;
+
+// SET Attribute
+// v = document.getElementById("header").innerHTML="<h1>Reboot</h1>";
+// document.getElementById("header").setAttribute("class", "textc");
+// document.getElementById("header").setAttribute("style", "border: 5px solid yellow;");
+// v = document.getElementById("header").getAttribute("class");
+// v = document.getElementById("header").attributes[3].value = "evaly";
+// document.getElementById("header").setAttribute("style", "border: 5px solid yellow;");
+// v = document.getElementById("header").getAttribute("class");
+
+// document.getElementById("header").removeAttribute("style");
+// v = document.getElementById("header").getAttribute("class");
+
+
+//QuerySelector
+// document.querySelector("#header").innerHTML="<h1>Eliana</h1>"
+// v = document.querySelector("#header").getAttribute("class");
+
+
+// v = document.querySelector("#header").style.border;
+// document.querySelector("#header").style.backgroundColor="white";
+
+// document.querySelector("#header").classList.add("headcolor");
+// v = document.querySelector("#header").classList;
+
+// Parent Method Node & Element
+// document.getElementById("left-content").parentElement.style.background="white";
+// v = document.getElementById("wrap-content").parentElement;
+// v = document.body.parentElement;
+// v = document.getElementById("main").parentElement;
+// v = document.getElementById("main").parentNode;
+
+// Children and childNodes
+// v = document.getElementById("left-content").children;
+// v = document.getElementById("left-content").children[1].innerHTML;
+// v = document.getElementById("left-content").children[1].style.background="white";
+// v = document.getElementById("left-content").children[1].style.padding="5px 0px";
+
+// v = document.getElementById("left-content").childNodes;
+// v = document.getElementById("left-content").childNodes[1];
+// v = document.getElementById("left-content").childNodes[1].innerHTML;
+// v = document.getElementById("left-content").childNodes[1].style.borderBottom = "2px solid white";
+
+// FirstChile & LastChild (ElementChild)
+// v = document.getElementById("left-content").firstElementChild;
+// v = document.getElementById("left-content").firstElementChild.style.background="white";
+// v = document.getElementById("left-content").firstElementChild.innerHTML;
+// v = document.getElementById("left-content").firstChild;
+
+// v = document.getElementById("left-content").lastElementChild;
+// v = document.getElementById("left-content").lastElementChild.innerHTML;
+// v = document.getElementById("left-content").lastElementChild.style.background="crimson";
+// v = document.getElementById("left-content").lastChild;
+
+// NextSibling & PreviousSibling (with Element)
+// v = document.getElementById("serve").nextElementSibling;
+// v = document.getElementById("serve").nextElementSibling.innerHTML;
+// v = document.getElementById("serve").nextElementSibling.style.background="white";
+// v = document.getElementById("serve").nextSibling;
+
+// v = document.getElementById("serve").previousElementSibling;
+// v = document.getElementById("serve").previousElementSibling.innerHTML;
+// v = document.getElementById("serve").previousElementSibling.style.background="yellow";
+// v = document.getElementById("serve").previousSibling;
+
+
+
+
+console.log(v);
+
+
